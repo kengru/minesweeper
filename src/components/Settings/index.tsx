f613a3af526e63f97c1ea28e64826a87b3ba6e40
Kendry Grullon <kengrullon@gmail.com>
2020-04-28 16:41:40 -0400

    Half styling the settings.

--- src/components/Settings/index.tsx
+++ src/components/Settings/index.tsx
@@ -7,33 +7,62 @@ const styles = StyleSheet.create({
   settings: {
     height: "100%",
     width: "15%"
-  }
+  },
+  form: {
+    display: "flex",
+    height: "100%",
+    marginLeft: "2em",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start"
+  },
+  radio: {
+    margin: "2em 0em 2em 0em"
+  },
+  radioLabel: {
+    display: "flex",
+    alignItems: "center",
+    fontSize: "1.5em"
+  },
+  radioInput: {
+    height: "2.2em",
+    width: "2.2em",
+    margin: "0em 1em 0em 0em"
+  },
 });
 
 interface Props {
   level: Levels;
   changeDifficulty: (level: Levels) => void;
-};
+}
 
 export const Settings = (props: Props) => {
   const onChangeRadio = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.changeDifficulty(Levels[event.currentTarget.value as keyof typeof Levels]);
-  }
+    props.changeDifficulty(
+      Levels[event.currentTarget.value as keyof typeof Levels]
+    );
+  };
 
   const levels = () => {
-    return Object.values(Levels).map(level => (
-      <div key={level} className="radio">
-        <label>
-          <input type="radio" value={level} checked={props.level === level} onChange={onChangeRadio} />
-            {level}
+    return Object.values(Levels).map((level) => (
+      <div key={level} className={css(styles.radio)}>
+        <label className={css(styles.radioLabel)}>
+          <input
+            type="radio"
+            className={css(styles.radioInput)}
+            value={level}
+            checked={props.level === level}
+            onChange={onChangeRadio}
+          />
+          {level}
         </label>
       </div>
     ));
-  }
+  };
 
   return (
     <div className={css(styles.settings)}>
-      <form>{levels()}</form>
+      <form className={css(styles.form)}>{levels()}</form>
     </div>
-  )
+  );
 };
